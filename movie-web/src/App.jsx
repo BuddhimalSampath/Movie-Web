f4df759333ede7b18732cbeb0f8063735c6fcee2
[Buddhimal Sampath] <[buddhimalsampath2001@gmail.com]>
2026-02-02 23:42:15 +0530

    setup tailwind css

--- movie-web/src/App.jsx
+++ movie-web/src/App.jsx
@@ -6,7 +6,9 @@ import './App.css'
 const App = () => {
   return (
     <div>
-      <h1>hello world</h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
